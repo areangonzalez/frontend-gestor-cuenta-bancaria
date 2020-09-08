5324fdca12baf64308a986b04ceb4569c33408af
areangonzalez <areangonzalez@gmail.com>
2020-09-08 17:14:29 -0300

    TG-64 #Closed se configura el jwt-interceptor para el token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,12 @@ import { NgbModule, NgbDatepickerI18n, NgbDateParserFormatter } from '@ng-bootst
 import { CustomDatepickerI18n, NgbDateARParserFormatter, FakeBackendInterceptor } from './core/helpers';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { JwtInterceptor, ErrorInterceptor } from './core/helpers';
+
 import { AppComponent } from './app.component';
 import { SharedModule, SistemaComponent, LoginComponent } from './shared';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 registerLocaleData(es);
 
@@ -27,6 +31,9 @@ registerLocaleData(es);
     { provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n },
     { provide: NgbDateParserFormatter, useClass: NgbDateARParserFormatter },
     { provide: LOCALE_ID, useValue: 'es-AR' },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
     // fake-backend
     FakeBackendInterceptor
   ],
--- src/app/core/helpers/index.ts
+++ src/app/core/helpers/index.ts
@@ -2,3 +2,4 @@ export * from './i18n_values';
 export * from './ngb-date-ar-parse-formatter';
 export * from './fake-backend-interceptor';
 export * from './error-interceptor';
+export * from './jwt-interceptor';
