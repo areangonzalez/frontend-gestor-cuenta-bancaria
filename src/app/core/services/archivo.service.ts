573768e248621863dbadce6da97b64a1e8decc0b
areangonzalez <areangonzalez@gmail.com>
2020-09-14 14:04:13 -0300

    TG-85 #Closed se crea el servicio para la descarga

---
+++ src/app/core/services/archivo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from "./api.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArchivoService {
+
+  constructor(private _http: ApiService) { }
+
+  exportarCtaSaldo(params: any) {
+    return this._http.getFile("/export/cta-saldo", params);
+  }
+
+
+  /* descargarExcel(params) {
+    let headers = new Headers();
+    let httpParams = new HttpParams();
+    httpParams = this._apiService.formatParams(httpParams, params);
+    headers.append('Content-type', 'aplication/json');
+    headers.append('Accept', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+    let options: object = {
+      responseType: 'blob',
+      params: httpParams,
+    };
+    return this._apiService.getFile('/export/exportar-prestaciones-xls', options);
+  } */
+}
