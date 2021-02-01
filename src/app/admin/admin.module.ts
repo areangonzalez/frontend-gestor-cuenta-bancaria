18c33da3442471b21aea092eeb436118e8f0fdb6
areangonzalez <areangonzalez@gmail.com>
2021-02-01 11:20:20 -0300

    TG-307 #Closed se crea el componente de busqueda avanzada

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from "../shared";
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { UsuariosComponent } from './componentes/lista/usuarios.component';
+import { UsuariosComponent, BusquedaAvanzadaComponent } from './componentes';
 
 
 @NgModule({
-  declarations: [AdminComponent, UsuariosComponent],
+  declarations: [AdminComponent, UsuariosComponent, BusquedaAvanzadaComponent],
   imports: [
     CommonModule,
     SharedModule,
