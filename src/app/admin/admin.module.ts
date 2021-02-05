b9737fdcab95fcf7baaad2d38b535bd7261b3c5e
areangonzalez <areangonzalez@gmail.com>
2021-02-05 11:17:06 -0300

    TG-320 se crea el formulario datos-usuario

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,10 +5,11 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { UsuariosComponent, BusquedaAvanzadaComponent, DatosPersonaComponent } from './componentes';
 import { GestorUsuarioComponent } from './gestor-usuario';
+import { DatosUsuarioComponent } from './componentes/form/datos-usuario.component';
 
 
 @NgModule({
-  declarations: [AdminComponent, UsuariosComponent, BusquedaAvanzadaComponent, GestorUsuarioComponent, DatosPersonaComponent],
+  declarations: [AdminComponent, UsuariosComponent, BusquedaAvanzadaComponent, GestorUsuarioComponent, DatosPersonaComponent, DatosUsuarioComponent],
   imports: [
     NgbModule,
     SharedModule,
