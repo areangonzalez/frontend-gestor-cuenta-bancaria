5e08086e7dc62d7e380c50d49e1fe4f050511303
areangonzalez <areangonzalez@gmail.com>
2021-03-10 13:28:04 -0300

    sin fake

--- src/app/core/helpers/error-interceptor.ts
+++ src/app/core/helpers/error-interceptor.ts
@@ -21,7 +21,7 @@ export class ErrorInterceptor implements HttpInterceptor {
             if (err.status === 401) {
                 // auto logout if 401 response returned from api
                 this._auth.logout();
-                location.reload(true);
+                //location.reload(true);
             }
             if (err.status === 403) {
               // auto logout if 401 response returned from api
