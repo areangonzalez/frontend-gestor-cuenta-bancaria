301b24995018ffab217a2a1d48e279677206eb1f
areangonzalez <areangonzalez@gmail.com>
2021-03-10 13:34:21 -0300

    sin fake

--- src/app/core/helpers/jwt-interceptor.ts
+++ src/app/core/helpers/jwt-interceptor.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { JwtService } from '../services';
+import { AutenticacionService } from '../services';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private _jwtService: JwtService) { }
+    constructor(private _auth: AutenticacionService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add auth header with jwt if user is logged in and request is to the api url
-        let currentUser = this._jwtService.getToken();
-        const isLoggedIn = currentUser && currentUser.datosToken && currentUser.datosToken.token;
-        if (isLoggedIn) {
+        // add authorization header with jwt token if available
+        let currentUser = this._auth.loggedIn;
+        // verifico autenticacion del usuario
+        // Y seteo el headers con el token
+        if (currentUser && currentUser.access_token) {
             request = request.clone({
                 setHeaders: {
-                  Authorization: `Bearer ${currentUser.datosToken.token}`
+                    Authorization: `Bearer ${currentUser.access_token}`
                 }
             });
         }
