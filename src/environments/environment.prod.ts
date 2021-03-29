0499154f47eb183398c8b55cc260e00f1f7d9a1b
areangonzalez <areangonzalez@gmail.com>
2021-03-29 13:26:19 -0300

    Se actualiza el icono del sistema

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+/**
+ * ultimo commit 7678eb26e4f3002cd929ee9960af67b152abd79b
+ */
 export const environment = {
   production: true,
-  apiUrl: 'https://gestor-bancario-api.rionegro.com.ar'
+  apiUrl: 'http://api.gcb.local'
 };
