9fc499e6462914681d15cdca608826614662e873
areangonzalez <areangonzalez@gmail.com>
2021-05-05 11:41:48 -0300

    se corrige notificacion

--- src/app/shared/notificacion/notificacion.component.ts
+++ src/app/shared/notificacion/notificacion.component.ts
@@ -53,6 +53,7 @@ export class NotificacionComponent implements OnInit {
   }
 
   crearNotificacion(arrMsj: any) {
+    console.log("mensaje de carlitos: ", arrMsj);
     this.mensaje = "Se han importado " + arrMsj["creadas"] + " cuenta/s en el sistema.";
     this.existen = (arrMsj["existen"]) ? "Se han encontrado " + arrMsj["existen"] + " cuenta/s que ya existen dentro del sistema.": false;
     this.errors = (arrMsj.errors !== undefined) ? arrMsj.errors : [];
