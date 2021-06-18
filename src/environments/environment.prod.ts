9d39dd054d775ded41e07082cfff2345b32bb14a
areangonzalez <areangonzalez@gmail.com>
2021-06-18 10:56:27 -0300

    se aplica disenio para mostrar la cantidad de registros

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@
  */
 export const environment = {
   production: true,
-  apiUrl: 'http://gcb.local'
+  apiUrl: 'http://api.gcb.local'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: '/apimock'
+  apiUrl: 'http://api.gcb.local'
 };
 
 /*
