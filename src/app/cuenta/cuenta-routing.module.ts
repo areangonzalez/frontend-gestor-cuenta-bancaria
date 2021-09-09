7de6fed0ad072a4556c89f7622c82b8538965020
areangonzalez <areangonzalez@gmail.com>
2021-09-09 13:45:10 -0300

    TG-585 #Closed se aplica los cambios de nombres en titulos de cuenta

--- src/app/cuenta/cuenta-routing.module.ts
+++ src/app/cuenta/cuenta-routing.module.ts
@@ -14,14 +14,14 @@ const routes: Routes = [
     children: [
       {
         path: 'alta', component: AltaCuentaPersonaComponent,
-        data: { title: 'Alta de Cuenta Saldo', breadcrumb: 'Alta de Cuenta Saldo', rol: ['usuario', 'admin'] },
+        data: { title: 'Alta de Convenio', breadcrumb: 'Alta de Convenio', rol: ['usuario', 'admin'] },
         resolve: {
           personas: PersonaService, subSucursales: SubSucursalService, seleccionPersona: CuentaSaldoService, tipoDocumentos: TipoDocumentoService, localidades: LocalidadService, nacionalidades: NacionalidadService, estadoCiviles: EstadoCivilService, sexos: SexoService, generos: GeneroService, bancos: BancoServiceService
         }
       },
       {
         path: 'importacion', component: ImportacionCbuComponent,
-        data: { title: 'Importación de Cuenta BPS', breadcrumb: 'Importación de Cuenta BPS', rol: ['usuario', 'admin'] },
+        data: { title: 'Importación de Cuentas', breadcrumb: 'Importación de Cuentas', rol: ['usuario', 'admin'] },
         resolve: {
           localidades: LocalidadService, personas: ArchivoService, tipoDocumentos: TipoDocumentoService, nacionalidades: NacionalidadService, estadoCiviles: EstadoCivilService, sexos: SexoService, generos: GeneroService, bancos: BancoServiceService
         }
