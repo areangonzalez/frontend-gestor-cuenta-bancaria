05eac7e248c265cb748badfe2814befc997ef1bd
areangonzalez <areangonzalez@gmail.com>
2021-09-24 08:51:40 -0300

    se cambia el routing

--- src/app/cuenta/cuenta-routing.module.ts
+++ src/app/cuenta/cuenta-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
 import { CuentaComponent } from './cuenta.component';
 import { AltaCuentaPersonaComponent, ImportacionCbuComponent } from './menu';
-import { BancoServiceService, ConvenioService, CuentaSaldoService, EstadoCivilService, GeneroService, LocalidadService, NacionalidadService, PersonaService, SexoService, SubSucursalService, TipoDocumentoService } from '../core/services';
+import { BancoServiceService, ConvenioService, CuentaSaldoService, EstadoCivilService, GeneroService, LocalidadService, NacionalidadService, PersonaService, PrestacionService, SexoService, SubSucursalService, TipoDocumentoService } from '../core/services';
 import { AuthGuard } from '../core/guards/auth.guard';
 import { ArchivoService } from '../core/services/archivo.service';
 
@@ -16,7 +16,7 @@ const routes: Routes = [
         path: 'alta', component: AltaCuentaPersonaComponent,
         data: { title: 'Alta de Convenio', breadcrumb: 'Alta de Convenio', rol: ['usuario','usuario_8180','usuario_8277','admin'] },
         resolve: {
-          personas: PersonaService, subSucursales: SubSucursalService, seleccionPersona: CuentaSaldoService, tipoDocumentos: TipoDocumentoService, localidades: LocalidadService, nacionalidades: NacionalidadService, estadoCiviles: EstadoCivilService, sexos: SexoService, generos: GeneroService, bancos: BancoServiceService, convenios: ConvenioService
+          personas: PersonaService, subSucursales: SubSucursalService, seleccionPersona: PrestacionService, tipoDocumentos: TipoDocumentoService, localidades: LocalidadService, nacionalidades: NacionalidadService, estadoCiviles: EstadoCivilService, sexos: SexoService, generos: GeneroService, bancos: BancoServiceService, convenios: ConvenioService
         }
       },
       {
@@ -34,6 +34,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [CuentaSaldoService, PersonaService, SubSucursalService, LocalidadService, NacionalidadService, PersonaService, SexoService, SubSucursalService, TipoDocumentoService, BancoServiceService, ConvenioService]
+  providers: [CuentaSaldoService, PersonaService, SubSucursalService, LocalidadService, NacionalidadService, PersonaService, SexoService, SubSucursalService, TipoDocumentoService, BancoServiceService, ConvenioService, PrestacionService]
 })
 export class CuentaRoutingModule { }
