19cacff7522ff00caffe64c7709281a3ee4c59d2
areangonzalez <areangonzalez@gmail.com>
2021-09-29 09:36:57 -0300

    se corrige los errores en edicion de permisos

--- src/app/admin/componentes/form/administrar-rol-permiso.component.ts
+++ src/app/admin/componentes/form/administrar-rol-permiso.component.ts
@@ -52,12 +52,16 @@ export class AdministrarRolPermisoComponent implements OnInit {
       this._msj.cancelado("No se ha seleccionado ningun permiso.");
       return;
     }else{
+      // si es un editado armo el array que se debe guardar
       if (this.editado) {
         for (const key in this.permisosSeleccionados) {
-          permisos.push({name: this.permisosSeleccionados[key]});
+          if (this.permisosSeleccionados[key].name == undefined) {
+            permisos.push({name: this.permisosSeleccionados[key]});
+          }else{
+            this.editado = false;
+          }
         }
       }
-
 
       let params: any  = {
         usuarioid: this.idUsuario,
@@ -77,15 +81,27 @@ export class AdministrarRolPermisoComponent implements OnInit {
       respuesta => {
         this._msj.exitoso("Se han agregado correctamente el convenio y los permisos al usuario.");
         this.obtenerListaPermisos(this.idUsuario);
+        this.editado = false;
+        this.limpiarForm();
       }, error => { this._msj.cancelado(error); }
     );
   }
-
+  /**
+   * Se editan los permisos dinamicamente
+   * @param permisos los datos del ususario con convenio y permisos
+   */
   editarPermisos(permisos: any){
-    console.log(permisos);
     this.datos.patchValue({tipo_convenioid: permisos.tipo_convenioid});
     this.permisosSeleccionados = permisos.lista_permiso;
-    let editado = true;
+    this.editado = true;
+  }
+  /**
+   * se limpia el formulario y listado
+   */
+  limpiarForm() {
+    this.datos.reset();
+    this.permisosSeleccionados = [];
+    this.datos.patchValue({tipo_convenioid: ""});
   }
 
 }
